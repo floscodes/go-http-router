3ebcb40512f557fcef870479ebe522c86de05ec7
flopetautschnig <69145145+floscodes@users.noreply.github.com>
2023-01-01 12:59:02 +0100

    Delete src directory

--- src/error_fns.rs
+++
@@ -1,9 +0,0 @@
-use crate::*;
-
-pub (in super) fn forbidden() -> Response<Body> {
-    Response::builder().status(StatusCode::FORBIDDEN).body(Body::empty()).unwrap()
-}
-
-pub (in super) fn notfound() -> Response<Body> {
-    Response::builder().status(StatusCode::NOT_FOUND).body(Body::empty()).unwrap()
-}
